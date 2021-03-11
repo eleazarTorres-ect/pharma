69749d6cd54366450890882e3b74ecd3b1dcf0e6
Eleazar Torres <eleazar.cacdao.torres@gmail.com>
2021-03-11 13:11:44 -0800

    Add scrolling effect when clicking on nav bar links

--- src/components/navigationSection/NavigationBar.jsx
+++ src/components/navigationSection/NavigationBar.jsx
@@ -8,6 +8,54 @@ const NavigationBar = () => {
     "px-2 py-2  sm:py-4 sm:px-24 md:px-24 lg:px-48  shadow-md border-bottom-2  ";
   const navDisplay =
     "fixed z-50 flex justify-center md:justify-end w-full bg-white ";
+
+  $(window).scroll(function () {
+    var home = $("#home").offset().top;
+    var aboutUs = $("#aboutUs").offset().top;
+    var service = $("#services").offset().top;
+    var blog = $("#blog").offset().top - 100;
+    var contactUs = $("#contactUs").offset().top - 540;
+    if ($(window).scrollTop() >= home) {
+      $("#homeNav").addClass("public_semibold border-b-2 ");
+      $("#aboutUsNav").removeClass("public_semibold border-b-2");
+      $("#servicesNav").removeClass("public_semibold border-b-2");
+      $("#projectsNav").removeClass("public_semibold border-b-2");
+      $("#blogNav").removeClass("public_semibold border-b-2");
+      $("#contactUsNav").removeClass("public_semibold border-b-2");
+    }
+    if ($(window).scrollTop() >= aboutUs) {
+      $("#homeNav").removeClass("public_semibold border-b-2 ");
+      $("#aboutUsNav").addClass("public_semibold border-b-2");
+      $("#servicesNav").removeClass("public_semibold border-b-2");
+      $("#projectsNav").removeClass("public_semibold border-b-2");
+      $("#blogNav").removeClass("public_semibold border-b-2");
+      $("#contactUsNav").removeClass("public_semibold border-b-2");
+    }
+    if ($(window).scrollTop() >= service) {
+      $("#homeNav").removeClass("public_semibold border-b-2 ");
+      $("#aboutUsNav").removeClass("public_semibold border-b-2");
+      $("#servicesNav").addClass("public_semibold border-b-2");
+      $("#projectsNav").removeClass("public_semibold border-b-2");
+      $("#blogNav").removeClass("public_semibold border-b-2");
+      $("#contactUsNav").removeClass("public_semibold border-b-2");
+    }
+    if ($(window).scrollTop() >= blog) {
+      $("#homeNav").removeClass("public_semibold border-b-2 ");
+      $("#aboutUsNav").removeClass("public_semibold border-b-2");
+      $("#servicesNav").removeClass("public_semibold border-b-2");
+      $("#projectsNav").removeClass("public_semibold border-b-2");
+      $("#blogNav").addClass("public_semibold border-b-2");
+      $("#contactUsNav").removeClass("public_semibold border-b-2");
+    }
+    if ($(window).scrollTop() >= contactUs) {
+      $("#homeNav").removeClass("public_semibold border-b-2 ");
+      $("#aboutUsNav").removeClass("public_semibold border-b-2");
+      $("#servicesNav").removeClass("public_semibold border-b-2");
+      $("#projectsNav").removeClass("public_semibold border-b-2");
+      $("#blogNav").removeClass("public_semibold border-b-2");
+      $("#contactUsNav").addClass("public_semibold border-b-2");
+    }
+  });
   return (
     <nav className={navDisplay + navPadding}>
       <div className="flex flex-grow items-center hidden md:flex">
