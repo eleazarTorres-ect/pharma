3df64d3dbc823cca7df946e519a86b780ed20718
Eleazar Torres <eleazar.cacdao.torres@gmail.com>
2021-03-11 10:09:46 -0800

    Update id of the About Us Container

--- src/components/aboutUsSection/AboutUsContainer.jsx
+++ src/components/aboutUsSection/AboutUsContainer.jsx
@@ -7,7 +7,7 @@ const AboutUsContainer = () => {
       className="h-auto lg:screenFromNav  w-full flex flex-col-reverse sm:flex-row justify-between
                 py-20 sm:py-20 
                 px-2 sm:px-24 md:px-24 lg:px-48"
-      id="home"
+      id="aboutUs"
     >
       <LeftContainer />
       <RightContainer />
