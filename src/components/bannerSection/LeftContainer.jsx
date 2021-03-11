fb36a0c266938df3222d8a2fd945dfe357bc8be8
Eleazar Torres <eleazar.cacdao.torres@gmail.com>
2021-03-11 10:01:47 -0800

    Fix mobile responsive on Banner

--- src/components/bannerSection/LeftContainer.jsx
+++ src/components/bannerSection/LeftContainer.jsx
@@ -4,22 +4,22 @@ const LeftContainer = () => {
   return (
     <div className="flex flex-wrap content-center w-full lg:w-1/2">
       <div>
-        <div className="public_regular text-3xl text-center lg:text-left">
+        <div className="public_regular text-3xl text-center md:text-left">
           Welcome to
         </div>
         <div className="flex flex-wrap justify-center lg:justify-start">
           <img className="h-16 -ml-2" src={Logo1} />
         </div>
-        <div className="public_regular text-lg lg:text-xl text-center lg:text-left">
+        <div className="public_regular text-lg lg:text-xl text-center md:text-left">
           Your All-in-One Pharmaceutical Website
         </div>
-        <div className="public_regular mt-2 text-sm text-pharma-B-300 text-center lg:text-left">
+        <div className="public_regular mt-2 text-sm text-pharma-B-300 text-center md:text-left">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat.
         </div>
-        <div className="flex flex-wrap justify-center lg:justify-start">
+        <div className="flex flex-wrap justify-center md:justify-start">
           <SubscriptionComponent />
         </div>
       </div>
