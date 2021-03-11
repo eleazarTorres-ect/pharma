b9e0b74d985f0cc38ff83188b760c896661a1bac
Eleazar Torres <eleazar.cacdao.torres@gmail.com>
2021-03-11 10:25:27 -0800

    Add Services Container and Mobile Responsive

---
+++ src/components/servicesSection/ServicesContainer.jsx
@@ -0,0 +1,48 @@
+import ServicesComponent from "./SerivcesComponent";
+
+const ServicesContainer = () => {
+  return (
+    <div
+      className="h-auto w-full flex flex-wrap justify-center
+                  py-10
+                  px-2 sm:px-24 md:px-24 lg:px-48"
+      id="generalFlow"
+    >
+      <div className="public_semibold  justify-center text-center text-pharma-A-400 text-3xl">
+        What We Offer
+      </div>
+      <div className="flex flex-wrap justify-center">
+        <ServicesComponent
+          id="1"
+          title="Quick Search"
+          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Voluptatibus quia, nulla! Maiores et perferendis eaque,
+        exercitationem praesentium nihil."
+        ></ServicesComponent>
+        <ServicesComponent
+          id="2"
+          title="Home Delivery"
+          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Voluptatibus quia, nulla! Maiores et perferendis eaque,
+        exercitationem praesentium nihil."
+        ></ServicesComponent>
+        <ServicesComponent
+          id="3"
+          title="Different Payment Methods"
+          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Voluptatibus quia, nulla! Maiores et perferendis eaque,
+        exercitationem praesentium nihil."
+        ></ServicesComponent>
+        <ServicesComponent
+          id="4"
+          title="24/7 Support"
+          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Voluptatibus quia, nulla! Maiores et perferendis eaque,
+        exercitationem praesentium nihil."
+        ></ServicesComponent>
+      </div>
+    </div>
+  );
+};
+
+export default ServicesContainer;
