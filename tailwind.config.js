016956456ddf89c051f0621ccb9998538334ac7c
Eleazar Torres <eleazar.cacdao.torres@gmail.com>
2021-03-11 23:08:32 -0800

    Add animation on scroll

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ module.exports = {
        keyframes: {
         bounce_idle: {
           '0%, 100%': { 
-            transform: 'translateY(-25%)',
+            transform: 'translateY(-5%)',
             animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)'
            },
           '50%': { 
